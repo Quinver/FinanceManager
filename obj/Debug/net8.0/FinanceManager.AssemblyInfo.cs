2669362dee41bd393f663e795183622fabe7a743
quinver150 <quinver15@gmail.com>
2024-08-12 21:30:01 +0200

    Ready to pull

--- obj/Debug/net8.0/FinanceManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/FinanceManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecbb44c2bd3343748967efa9c1e334cc92ea2bfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e30157f8be4a558ffa5f810555533470e9907d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
